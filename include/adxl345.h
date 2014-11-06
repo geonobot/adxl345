b01e37640363a5a01b02eebca3fc6a26097392d5
ldo <laur.dupont@gmail.com>
2014-11-06 10:17:27 +0100

    add "low_pass_filter" function complete "setup" function

--- include/adxl345.h
+++ include/adxl345.h
@@ -13,8 +13,12 @@
 #include "twi.h"
 #include "imu.h"
 
-#define ADXL345_ADDRESS 0x53 // 7-bits i2c address
+#define ADXL345_ADDRESS_LOW 0x53 // if SDO = 0 then ADXL345 7-bits address is 0x53
+#define ADXL345_ADDRESS_HIGH 0x1D // if SDO = 1 then ADXL345 7-bits address is 0x1D
 
+#define SENSITIVITY_2g 256.0 // for 2g, the sensitivity is 256 LSB/g
+#define SENSITIVITY_4g 128.0 // for 16g, the sensitivity is 128 LSB/g
+#define SENSITIVITY_8g 64.0 // for 16g, the sensitivity is 64 LSB/g
 #define SENSITIVITY_16g 32.0 // for 16g, the sensitivity is 32 LSB/g
 
 /* Register map */
@@ -35,13 +39,20 @@
 #define MEASURE 0x08
 
 /* DATA FORMAT register */
+#define G_RANGE_2g 0x00
+#define G_RANGE_4g 0x01
 #define G_RANGE_8g 0x02
 #define G_RANGE_16g 0x03
 #define FULL_RES 0x04
 #define SELF_TEST 0x80
 
+typedef enum
+{
+  MR_2_g = 0, MR_4_g = 1, MR_8_g = 2, MR_16_g = 3
+} adxl345_measurement_range;
+
 void
-adxl345_setup (void);
+adxl345_setup (uint8_t sdo, adxl345_measurement_range mr);
 void
 adxl345_power_on (void);
 
@@ -54,7 +65,7 @@ adxl345_read_device_id (void);
 void
 adxl345_write_offset_xyz (xyz offset);
 xyz
-adxl345_read_offset_xyz(void);
+adxl345_read_offset_xyz (void);
 
 void
 adxl345_write_data_format (void);
--- src/adxl345.c
+++ src/adxl345.c
@@ -10,16 +10,66 @@
 
 #include "adxl345.h"
 
-uint16_t sensitivity;
+static uint16_t sensitivity = SENSITIVITY_16g;
+static uint8_t slave_address = ADXL345_ADDRESS_LOW;
+
+static xyz low_pass;
 
 /*
  * \fn void adxl345_setup (void)
  * \brief adxl345 setup function
+ * \param sdo select slave address
+ * \param mr full-scale range (2, 4, 8 or 16 g)
  * */
 void
-adxl345_setup (void)
+adxl345_setup (uint8_t sdo, adxl345_measurement_range mr)
 {
+  volatile uint8_t buffer[2];
+
   twi_master_setup (/*SCL_FREQ_100*/);
+
+  if (sdo == 0)
+    {
+      slave_address = ADXL345_ADDRESS_LOW;
+    }
+  else
+    {
+      slave_address = ADXL345_ADDRESS_HIGH;
+    }
+
+  /* set the g range (2, 4, 8 or 16 g) */
+  switch (mr)
+    {
+    case MR_2_g:
+      buffer[0] = DATA_FORMAT;
+      buffer[1] = G_RANGE_2g;
+      twi_write_bytes (slave_address, 2, buffer);
+      sensitivity = SENSITIVITY_2g;
+      break;
+    case MR_4_g:
+      buffer[0] = DATA_FORMAT;
+      buffer[1] = G_RANGE_4g;
+      twi_write_bytes (slave_address, 2, buffer);
+      sensitivity = SENSITIVITY_4g;
+      break;
+    case MR_8_g:
+      buffer[0] = DATA_FORMAT;
+      buffer[1] = G_RANGE_8g;
+      twi_write_bytes (slave_address, 2, buffer);
+      sensitivity = SENSITIVITY_8g;
+      break;
+    case MR_16_g:
+    default:
+      buffer[0] = DATA_FORMAT;
+      buffer[1] = G_RANGE_16g;
+      twi_write_bytes (slave_address, 2, buffer);
+      sensitivity = SENSITIVITY_16g;
+      break;
+    }
+
+  low_pass.x = 0.0;
+  low_pass.y = 0.0;
+  low_pass.z = 0.0;
 }
 
 /*
@@ -34,14 +84,23 @@ adxl345_power_on (void)
   /* setting of 1 in the measure bit places the part into measurement mode */
   buffer[0] = POWER_CTL;
   buffer[1] = MEASURE;
-  twi_write_bytes (ADXL345_ADDRESS, 2, buffer);
+  twi_write_bytes (slave_address, 2, buffer);
+}
 
-  /* these bits set the g range (2, 4, 8 or 16 g) */
-  buffer[0] = DATA_FORMAT;
-  buffer[1] = G_RANGE_16g;
-  twi_write_bytes (ADXL345_ADDRESS, 2, buffer);
+/*
+ * \fn uint8_t adxl345_read_device_id (void)
+ * \brief read device id
+ * \return a fixed device id code of 0xE5
+ * */
+uint8_t
+adxl345_read_device_id (void)
+{
+  volatile uint8_t reg = DEVICE;
+  volatile uint8_t device_id;
 
-  sensitivity = SENSITIVITY_16g; /* sensitivity for 16g full_scale range */
+  twi_read_bytes (slave_address, &reg, 1, &device_id);
+
+  return device_id;
 }
 
 /*
@@ -57,33 +116,17 @@ adxl345_self_test (void)
   /* self-test function is enable (SELF_TEST bit in DATA_FORMAT is set) */
   buffer[0] = DATA_FORMAT;
   buffer[1] = SELF_TEST + G_RANGE_16g;
-  twi_write_bytes (ADXL345_ADDRESS, 2, buffer);
+  twi_write_bytes (slave_address, 2, buffer);
 
   /* read x, y and y value */
-  acc = adxl345_read_acc_xyz();
+  acc = adxl345_read_acc_xyz ();
 
   /* self-test function is disable (SELF_TEST bit in DATA_FORMAT is reset) */
   buffer[0] = DATA_FORMAT;
   buffer[1] = G_RANGE_16g;
-  twi_write_bytes (ADXL345_ADDRESS, 2, buffer);
+  twi_write_bytes (slave_address, 2, buffer);
 
   return acc;
-}
-
-/*
- * \fn uint8_t adxl345_read_device_id (void)
- * \brief read device id
- * \return device id
- * */
-uint8_t
-adxl345_read_device_id (void)
-{
-  volatile uint8_t reg = DEVICE;
-  volatile uint8_t device_id;
-
-  twi_read_bytes (ADXL345_ADDRESS, &reg, 1, &device_id);
-
-  return device_id;
 }
 
 /*
@@ -98,7 +141,7 @@ adxl345_read_offset_xyz (void)
   volatile uint8_t buffer[3];
   volatile xyz offset;
 
-  twi_read_bytes (ADXL345_ADDRESS, &reg, 3, buffer);
+  twi_read_bytes (slave_address, &reg, 3, buffer);
 
   offset.x = buffer[0];
   offset.y = buffer[1];
@@ -122,7 +165,22 @@ adxl345_write_offset_xyz (xyz offset)
   buffer[2] = offset.y;
   buffer[3] = offset.z;
 
-  twi_write_bytes (ADXL345_ADDRESS, 4, buffer);
+  twi_write_bytes (slave_address, 4, buffer);
+}
+
+/**
+ * \brief low pass filter y(t) = a.xm(t) – (1-a).y(t-1)
+ * */
+void
+low_pass_filter (xyz *measure)
+{
+  double alpha = 0.2;
+
+  low_pass.x = measure->x * alpha + (low_pass.x * (1.0 - alpha));
+  low_pass.y = measure->y * alpha + (low_pass.y * (1.0 - alpha));
+  low_pass.z = measure->z * alpha + (low_pass.z * (1.0 - alpha));
+
+  *measure = low_pass;
 }
 
 /*
@@ -135,13 +193,15 @@ adxl345_read_acc_xyz (void)
 {
   volatile uint8_t reg = DATAX0;
   volatile uint8_t buffer[6];
-  volatile xyz acc;
+  xyz acc;
 
-  twi_read_bytes (ADXL345_ADDRESS, &reg, 6, buffer);
+  twi_read_bytes (slave_address, &reg, 6, buffer);
 
   acc.x = (double) (buffer[0] + (buffer[1] << 8)) / sensitivity;
   acc.y = (double) (buffer[2] + (buffer[3] << 8)) / sensitivity;
   acc.z = (double) (buffer[4] + (buffer[5] << 8)) / sensitivity;
+
+  low_pass_filter (&acc);
 
   return acc;
 }
